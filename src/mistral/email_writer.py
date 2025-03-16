82bbf5e7f6585e50a99ce862fb9c9006e1481c8d
nifleisch <nils.fleischmann@outlook.com>
2025-03-16 03:00:23 +0100

    feat: add supplier email response agent to agentically mock supplier responses

--- src/mistral/email_writer.py
+++ src/mistral/email_writer.py
@@ -1,5 +1,6 @@
 import json
 import pickle
+from time import sleep
 import pandas as pd
 import sys
 import os
@@ -9,6 +10,7 @@ from src.mistral.agent import agent_call
 
 
 email_agent_id = "ag:f9b7aa04:20250315:untitled-agent:d38f7f78"
+supplier_email_agent_id = "ag:f9b7aa04:20250316:supplier-email-response-agent:77065d2f"
 
 def read_offer_and_leverage_data():
     # mock this for now :D
@@ -70,7 +72,35 @@ def email_agent(supplier_name, accept: bool):
 
     return res_json
 
-def add_email_response(supplier_name, email):
+def supplier_email_agent(supplier_name, accept:bool):
+    # Check if there is a previous message thread for this supplier stored in a pickle file
+    if os.path.exists(f"email_messages/{supplier_name}_email_chat_history.pkl"):
+        with open(f"email_messages/{supplier_name}_email_chat_history.pkl", 'rb') as f:
+            chat_history = pickle.load(f)
+    else:
+        raise AssertionError(f"No previous message thread found for supplier {supplier_name}")
+
+    previous_message = json.loads(chat_history[-1].content).get('email')
+
+    message = f"You got an email from the customer. {'Accept the offer.' if accept else 'Do a counteroffer.'}\n\n{previous_message}"
+    response_format = {
+        "email": {
+            "type": "string",
+        },
+        "additionalProperties": False,
+        "required": ["email"]
+    }
+
+    res = agent_call(supplier_email_agent_id, message, response_format=response_format, tools=tools, tool_name_to_function=tool_name_to_function)
+    res_json = json.loads(res)
+    email = res_json.get('email')
+
+    _add_email_response(supplier_name, email)
+
+    return email
+
+
+def _add_email_response(supplier_name, email):
     # Check if there is a previous message thread for this supplier stored in a pickle file
     if os.path.exists(f"email_messages/{supplier_name}_email_chat_history.pkl"):
         with open(f"email_messages/{supplier_name}_email_chat_history.pkl", 'rb') as f:
@@ -86,27 +116,19 @@ def add_email_response(supplier_name, email):
 
     return
 
+def delete_email_history(supplier_name):
+    if os.path.exists(f"email_messages/{supplier_name}_email_chat_history.pkl"):
+        os.remove(f"email_messages/{supplier_name}_email_chat_history.pkl")
+    return
+
 if __name__ == '__main__':
     email_drivemaster_1 = email_agent("DriveMaster", accept=False).get('email')
-    response_email_1 = """Dear John Dough,
-
-thanks for your reply. I'm sorry, we can not meet your offer.
-We are willing to do 980$.
-
-Best regards,
-DriveMaster
-"""
-    add_email_response("DriveMaster", response_email_1)
-    email_drivemaster_2 = email_agent("DriveMaster", accept=False).get('email')
-    response_email_2 = """Dear John Dough,
-
-thanks for your reply. I'm glad, we can meet your offer.
-
-Best regards,
-DriveMaster
-    """
-    add_email_response("DriveMaster", response_email_2)
-    email_drivemaster_3 = email_agent("DriveMaster", accept=True).get('email')
+    sleep(5)
+    response_email_1 = supplier_email_agent("DriveMaster", accept=False)
+    sleep(5)
+    email_drivemaster_2 = email_agent("DriveMaster", accept=True).get('email')
+    sleep(5)
+    delete_email_history("DriveMaster")
 
     print('Email to DriveMaster 1:')
     print(email_drivemaster_1)
@@ -114,7 +136,3 @@ DriveMaster
     print(response_email_1)
     print('Email to DriveMaster 2:')
     print(email_drivemaster_2)
-    print('Response from DriveMaster:')
-    print(response_email_2)
-    print('Email to DriveMaster 3:')
-    print(email_drivemaster_3)
